33fe0a42f5e9bee299037c184c2cb6153c4ff0a8
Dain Sundstrom <dain@iq80.com>
2022-09-25 13:09:21 -0700

    Extract byte code generation from class generator

--- src/main/java/io/airlift/bytecode/ClassGenerator.java
+++ src/main/java/io/airlift/bytecode/ClassGenerator.java
@@ -16,19 +16,9 @@ package io.airlift.bytecode;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.reflect.Reflection;
-import org.objectweb.asm.ClassReader;
-import org.objectweb.asm.ClassWriter;
-import org.objectweb.asm.util.CheckClassAdapter;
-import org.objectweb.asm.util.Textifier;
-import org.objectweb.asm.util.TraceClassVisitor;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.StringWriter;
-import java.io.UncheckedIOException;
 import java.io.Writer;
 import java.lang.invoke.MethodHandle;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -36,20 +26,13 @@ import java.util.Map;
 import java.util.Optional;
 
 import static com.google.common.collect.MoreCollectors.onlyElement;
-import static com.google.common.io.CharStreams.nullWriter;
 import static io.airlift.bytecode.ClassInfoLoader.createClassInfoLoader;
-import static io.airlift.bytecode.ParameterizedType.typeFromJavaClassName;
-import static java.nio.file.Files.createDirectories;
 import static java.util.Objects.requireNonNull;
 
 public class ClassGenerator
 {
     private final DynamicClassLoader classLoader;
-    private final boolean fakeLineNumbers;
-    private final boolean runAsmVerifier;
-    private final boolean dumpRawBytecode;
-    private final Writer output;
-    private final Optional<Path> dumpClassPath;
+    private final ByteCodeGenerator byteCodeGenerator;
 
     public static ClassGenerator classGenerator(ClassLoader parentClassLoader)
     {
@@ -63,43 +46,33 @@ public class ClassGenerator
 
     public static ClassGenerator classGenerator(DynamicClassLoader classLoader)
     {
-        return new ClassGenerator(classLoader, false, false, false, nullWriter(), Optional.empty());
+        return new ClassGenerator(classLoader, ByteCodeGenerator.byteCodeGenerator());
     }
 
-    private ClassGenerator(
-            DynamicClassLoader classLoader,
-            boolean fakeLineNumbers,
-            boolean runAsmVerifier,
-            boolean dumpRawBytecode,
-            Writer output,
-            Optional<Path> dumpClassPath)
+    private ClassGenerator(DynamicClassLoader classLoader, ByteCodeGenerator byteCodeGenerator)
     {
         this.classLoader = requireNonNull(classLoader, "classLoader is null");
-        this.fakeLineNumbers = fakeLineNumbers;
-        this.runAsmVerifier = runAsmVerifier;
-        this.dumpRawBytecode = dumpRawBytecode;
-        this.output = requireNonNull(output, "output is null");
-        this.dumpClassPath = requireNonNull(dumpClassPath, "dumpClassPath is null");
+        this.byteCodeGenerator = requireNonNull(byteCodeGenerator, "byteCodeGenerator is null");
     }
 
     public ClassGenerator fakeLineNumbers(boolean fakeLineNumbers)
     {
-        return new ClassGenerator(classLoader, fakeLineNumbers, runAsmVerifier, dumpRawBytecode, output, dumpClassPath);
+        return new ClassGenerator(classLoader, byteCodeGenerator.fakeLineNumbers(fakeLineNumbers));
     }
 
     public ClassGenerator runAsmVerifier(boolean runAsmVerifier)
     {
-        return new ClassGenerator(classLoader, fakeLineNumbers, runAsmVerifier, dumpRawBytecode, output, dumpClassPath);
+        return new ClassGenerator(classLoader, byteCodeGenerator.runAsmVerifier(runAsmVerifier ? classLoader : null));
     }
 
     public ClassGenerator dumpRawBytecode(boolean dumpRawBytecode)
     {
-        return new ClassGenerator(classLoader, fakeLineNumbers, runAsmVerifier, dumpRawBytecode, output, dumpClassPath);
+        return new ClassGenerator(classLoader, byteCodeGenerator.dumpRawBytecode(dumpRawBytecode));
     }
 
     public ClassGenerator outputTo(Writer output)
     {
-        return new ClassGenerator(classLoader, fakeLineNumbers, runAsmVerifier, dumpRawBytecode, output, dumpClassPath);
+        return new ClassGenerator(classLoader, byteCodeGenerator.outputTo(output));
     }
 
     public ClassGenerator dumpClassFilesTo(Path dumpClassPath)
@@ -109,7 +82,7 @@ public class ClassGenerator
 
     public ClassGenerator dumpClassFilesTo(Optional<Path> dumpClassPath)
     {
-        return new ClassGenerator(classLoader, fakeLineNumbers, runAsmVerifier, dumpRawBytecode, output, dumpClassPath);
+        return new ClassGenerator(classLoader, byteCodeGenerator.dumpClassFilesTo(dumpClassPath));
     }
 
     public <T> Class<? extends T> defineClass(ClassDefinition classDefinition, Class<T> superType)
@@ -124,50 +97,8 @@ public class ClassGenerator
         Map<String, byte[]> bytecodes = new LinkedHashMap<>();
 
         for (ClassDefinition classDefinition : classDefinitions) {
-            ClassWriter writer = new SmartClassWriter(classInfoLoader);
-
-            try {
-                classDefinition.visit(fakeLineNumbers ? new AddFakeLineNumberClassVisitor(writer) : writer);
-            }
-            catch (IndexOutOfBoundsException | NegativeArraySizeException e) {
-                StringWriter out = new StringWriter();
-                classDefinition.visit(new TraceClassVisitor(null, new Textifier(), new PrintWriter(out)));
-                throw new IllegalArgumentException("Error processing class definition:\n" + out, e);
-            }
-
-            byte[] bytecode;
-            try {
-                bytecode = writer.toByteArray();
-            }
-            catch (RuntimeException e) {
-                throw new CompilationException("Error compiling class: " + classDefinition.getName(), e);
-            }
-
+            byte[] bytecode = byteCodeGenerator.generateByteCode(classInfoLoader, classDefinition);
             bytecodes.put(classDefinition.getType().getJavaClassName(), bytecode);
-
-            if (runAsmVerifier) {
-                ClassReader reader = new ClassReader(bytecode);
-                CheckClassAdapter.verify(reader, classLoader, true, new PrintWriter(output));
-            }
-        }
-
-        dumpClassPath.ifPresent(path -> bytecodes.forEach((className, bytecode) -> {
-            String name = typeFromJavaClassName(className).getClassName() + ".class";
-            Path file = path.resolve(name).toAbsolutePath();
-            try {
-                createDirectories(file.getParent());
-                Files.write(file, bytecode);
-            }
-            catch (IOException e) {
-                throw new UncheckedIOException("Failed to write generated class file: " + file, e);
-            }
-        }));
-
-        if (dumpRawBytecode) {
-            for (byte[] bytecode : bytecodes.values()) {
-                ClassReader classReader = new ClassReader(bytecode);
-                classReader.accept(new TraceClassVisitor(new PrintWriter(output)), ClassReader.EXPAND_FRAMES);
-            }
         }
 
         Map<String, Class<?>> classes = classLoader.defineClasses(bytecodes);
